b4786c5694a31c398b369480d14db14c73276829
yoonsangeun1 <yoonsangeun1@gmail.com>
2019-12-03 13:40:45 +0900

    샘플데이터 insert

---
+++ movingMVC/src/main/webapp/resources/SQL/샘플_데이터.sql
@@ -0,0 +1,10 @@
+/* 코드_유형_마스터 */
+INSERT INTO code_type_master (code_type_master_seq.nextval, 100, '게시글', 'post', sysdate);
+INSERT INTO code_type_master (code_type_master_seq.nextval, 200, '프로젝트', 'project', sysdate);
+INSERT INTO code_type_master (code_type_master_seq.nextval, 300, '영상', 'video', sysdate);
+INSERT INTO code_type_master (code_type_master_seq.nextval, 500, '회원', 'user', sysdate);
+INSERT INTO code_type_master (code_type_master_seq.nextval, 800, '부가 기능', 'additional function', sysdate);
+
+/* 코드_마스터 */
+INSERT INTO code_master (code_master_seq.nextval, 100, '게시글', 'post', sysdate);
+
