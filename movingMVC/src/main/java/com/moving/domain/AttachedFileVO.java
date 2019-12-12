b46ebceeeaaa30bf8b729aa200cd82840b000091
chaehoon <chaehoon1919@gmail.com>
2019-12-12 16:06:48 +0900

    다중파일 업로드, 에디터 적용 했음

--- movingMVC/src/main/java/com/moving/domain/AttachedFileVO.java
+++ movingMVC/src/main/java/com/moving/domain/AttachedFileVO.java
@@ -1,5 +1,7 @@
 package com.moving.domain;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class AttachedFileVO {		/* 첨부_파일 */
 
 	private int id; 				/* 아이디 */
@@ -16,6 +18,34 @@ public class AttachedFileVO {		/* 첨부_파일 */
 	private int socialPostId; 		/* 소셜_게시글_아이디 */
 	private int replyPostId; 		/* 답글_게시글_아이디 */
 	
+	private MultipartFile Filedata; //photo_uploader.html페이지의 form태그내에 존재하는 file 태그의 name명과 일치시켜줌
+	private String callback; // callback url
+	private String callback_func; //콜백함수??
+	
+	public int getFileType() {
+		return fileType;
+	}
+	public void setFileType(int fileType) {
+		this.fileType = fileType;
+	}
+	public MultipartFile getFiledata() {
+		return Filedata;
+	}
+	public void setFiledata(MultipartFile filedata) {
+		Filedata = filedata;
+	}
+	public String getCallback() {
+		return callback;
+	}
+	public void setCallback(String callback) {
+		this.callback = callback;
+	}
+	public String getCallback_func() {
+		return callback_func;
+	}
+	public void setCallback_func(String callback_func) {
+		this.callback_func = callback_func;
+	}
 	public int getId() {
 		return id;
 	}
